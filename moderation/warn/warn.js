7a281abe738177f6fe27a40bf298ef7efbce5365
mudden88 <mudden@live.se>
2025-02-16 15:38:40 +0100

    warn feature comments

--- moderation/warn/warn.js
+++ moderation/warn/warn.js
@@ -13,6 +13,8 @@ module.exports = {
     data: new SlashCommandBuilder()
                 .setName('warn')
                 .setDescription('Warn a user, check their warnings, or remove a warning.')
+                
+                // Add a warning
                     .addSubcommand(subcommand => subcommand.setName('add')
                         .setName('add')
                         .setDescription('Warn a user.')
@@ -24,17 +26,28 @@ module.exports = {
                             option.setName("reason")
                                 .setDescription("Reason for the warning")
                                 .setRequired(false)))
+                                
+                                // Check a user's warnings
                     .addSubcommand(subcommand => subcommand.setName('check')
                         .setDescription('Check a user\'s warnings')
                         .addUserOption(option =>
                             option.setName("target")
                                 .setDescription("The user to check")
                                 .setRequired(true)))
+
+                                // Remove a user's warning
                     .addSubcommand(subcommand => subcommand.setName('remove')
                 .setDescription('Remove a user\'s warning')
                 .addUserOption(option => 
                     option.setName("target")
                         .setDescription("The user to remove a warning from")
                         .setRequired(true))),
+            
+
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+        const target = interaction.options.getUser("target");
+        const guildId = interaction.guild.id;
+    }
                    
-    };
+ };
